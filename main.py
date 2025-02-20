a94a164c75f05eacf88ea87808a115c1c77e0416
funda-metal <zachencinas@gmail.com>
2025-02-20 10:26:29 -0500

    Window successfully opens.

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import pygame
+from constants import *
 
 def main():
-    echo "Starting Asteroids!"
+    pygame.init
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while 1 < 2:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill(000)
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
